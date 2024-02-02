3c2e3a155d5d2adcddb53d396c70a9eec8858854
Shojn <158007140+Shojn@users.noreply.github.com>
2024-02-02 14:39:46 +0800

    Lozada.py

---
+++ Activity 3/Lozada.py
@@ -0,0 +1,5 @@
+if genre == "8":
+    print("Hello " + user + "! We recommend Action blockbuster Spiderman Homecoming. ")
+
+if genre == "7":
+    print("Hello " + user + "! We recommend Comedy hit Praybet Benjamen.")
